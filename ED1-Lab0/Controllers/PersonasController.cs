8c8fe71eb2327eefd06feb9a48a98c86e21d133c
engelsruiz09 <jaruizc@correo.url.edu.gt>
2023-02-01 08:15:29 -0600

    agregando gitignore y nuevo controller

---
+++ ED1-Lab0/Controllers/PersonasController.cs
@@ -0,0 +1,22 @@
+﻿using ED1_Lab0.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ED1_Lab0.Controllers
+{
+    [Route("[controller]")] // ("[persona]")] 
+    public class PersonasController : Controller
+    {
+        [HttpGet]
+        [Route("[ejemplopersona]")]
+        public Persona ObtenerPersona()
+        {
+            Persona ejemplo = new Persona()
+            {
+                Nombre = "chepe",
+                Profesion = "Medico",
+                Edad = 48
+            };
+            return ejemplo;
+        }
+    }
+}
